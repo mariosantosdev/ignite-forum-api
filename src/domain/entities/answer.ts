25f16611aa3b79c65842aab0fba47c97f57bc159
Mário Santos <mariodev7@gmail.com>
2023-07-24 09:57:19 -0300

    feat(answer): mapping props

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-ts"
 
 interface AnswerProps {
     content: string
-    authorId: string
-    questionId: string
+    authorId: UniqueEntityId
+    questionId: UniqueEntityId
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
