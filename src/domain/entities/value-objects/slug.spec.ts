e050eaafe4c4333164e4c82128cc74d4bf44d939
Mário Santos <mariodev7@gmail.com>
2023-07-24 09:41:52 -0300

    feat: value object of slug

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,10 @@
+import { expect, it, test } from "vitest";
+import { Slug } from "./slug";
+
+test('Slug value objects', () => {
+    it('it should be able to create a new slug from text', () => {
+        const slug = Slug.createFromText('Question title example')
+    
+        expect(slug.value).toEqual('question-title-example')
+    })
+})
