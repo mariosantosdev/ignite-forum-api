677d397d6c0dbeb271c4c76df2b065e0928e6b68
Mário Santos <mariodev7@gmail.com>
2023-07-24 09:57:24 -0300

    feat(question): mapping props

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-ts"
 
 interface QuestionProps {
+    authorId: UniqueEntityId
+    bestAnswerId?: UniqueEntityId
     title: string
     slug: Slug
     content: string
-    authorId: string
+    createdAt: Date
+    updateAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
