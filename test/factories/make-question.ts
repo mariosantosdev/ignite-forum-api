8f5f8b77efe78f3fd45c562adec2889b45164c65
Mário Santos <mariodev7@gmail.com>
2023-07-30 13:39:38 -0300

    test: generate fake data

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,14 +3,21 @@ import {
   Question,
   QuestionProps,
 } from '~/domain/forum/enterprise/entities/question'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps>) {
-  const question = Question.create({
-    content: 'Lorem ipsum',
-    title: 'Title',
-    authorId: new UniqueEntityId('1'),
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      content: faker.lorem.text(),
+      title: faker.lorem.sentence(),
+      authorId: new UniqueEntityId('1'),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
