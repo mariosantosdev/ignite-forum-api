0bb79fd29487e3b07c13b3b4ef7cc47575feda95
Mário Santos <mariodev7@gmail.com>
2023-07-30 15:15:48 -0300

    feat(use-case): create use-case comment-on-question

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment on Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on question', async () => {
+    const newQuestion = makeQuestion()
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: newQuestion.id.toString(),
+      authorId: newQuestion.authorId.toString(),
+      content: 'Comment test',
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'Comment test',
+    )
+  })
+})
---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,43 @@
+import { UniqueEntityId } from '~/core/entities/unique-entity-ts'
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repostitories/question-comments-repository'
+import { QuestionsRepository } from '../repostitories/question-repository'
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  content: string
+}
+
+interface CommentOnQuestionUseCaseResponse {
+  questionComment: QuestionComment
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionCommentsRepository: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    questionId,
+    content,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found')
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityId(authorId),
+      questionId: new UniqueEntityId(questionId),
+      content,
+    })
+
+    await this.questionCommentsRepository.create(questionComment)
+
+    return { questionComment }
+  }
+}
